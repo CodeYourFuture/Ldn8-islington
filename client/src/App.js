cf0743ad6f160b26f8b206ee70f12a4493112c4d
Davood Khoshnood <khoshnood1977@gmail.com>
2022-05-22 14:47:47 +0100

    New components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import './App.css'
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
-<div className="content">
-  <h1>Mini-guide</h1>
-</div>
+      <div className="content">
+        <h1>Mini-guide</h1>
+      </div>
+      <Main />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
